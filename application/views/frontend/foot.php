6738d7bba495e8a2aa44a1b283a9a604981c9541
sunil kumar mutaka <carbojet@gmail.com>
2021-03-19 11:10:53 +0530

    modified

--- application/views/frontend/foot.php
+++ application/views/frontend/foot.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <script type='text/javascript' src='<?php echo base_url();?>theme/frontend/js/autoNumeric.js'></script>
 <script type='text/javascript' src='<?php echo base_url();?>theme/frontend/js/jquery-ui.js'></script>
 <script type='text/javascript' src='<?php echo base_url();?>theme/frontend/js/custom-javascript.js'></script>
